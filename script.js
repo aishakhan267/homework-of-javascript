bd7e5cc99153bb873a207001c739240086052ae4
aishakhan267 <49521671+aishakhan267@users.noreply.github.com>
2019-07-11 02:22:05 +0500

    addedd

---
+++ script.js
@@ -0,0 +1,91 @@
+let circleOrganization = {
+    name:"Tech karo",
+    purpose:"Women Empowerment",
+    passedCohort:1,
+    isProgress:"true",
+    courseTitle:"Web Development",
+    currentStrength:100
+};
+
+let currentStrength = [
+    {
+        name:"Aisha",
+        contactNumber:"0333-8009665",
+        education:"Bachelor of Science",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:["Drawing", "Coding"]
+    },
+    {
+        name:"Sameer",
+        contactNumber:"0333-5007889",
+        education:"F.Sc",
+        hasPromoCode:true,
+        group:"A",
+        hobbies:["Riding", "Singing"]
+    },
+    {
+        name:"Saim",
+        contactNumber:"0300-5092789",
+        education:"F.Sc",
+        hasPromoCode:false,
+        group:"A",
+        hobbies:["Riding", "Painting", "walking"]
+    },
+    {
+        name:"Amertiya",
+        contactNumber:"0345-5007000",
+        education:"M.Sc",
+        hasPromoCode:true,
+        group:"A",
+        hobbies:["Cooking", "Singing"]
+    },
+    {
+        name:"Armia",
+        contactNumber:"0333-0908765",
+        education:"B.Sc",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:"Coding"
+    },
+    {
+        name:"Rubab",
+        contactNumber:"0333-7898900",
+        education:"Matriculation",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:"Coding"
+    },
+    {
+        name:"Maha",
+        contactNumber:"0343-5908705",
+        education:"B.Sc",
+        hasPromoCode:false,
+        group:"B",
+        hobbies:["Gardening", "Coding", "Cooking"]
+    },
+    {
+        name:"Sadia",
+        contactNumber:"0346-1908995",
+        education:"M.A",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:"Coding"
+    },
+    {
+        name:"Lubna",
+        contactNumber:"0300-0901769",
+        education:"B.Sc",
+        hasPromoCode:true,
+        group:"A",
+        hobbies:["Poerty", "Reading"]
+    },
+    {
+        name:"Sarfaraz",
+        contactNumber:"0343-0907775",
+        education:"M.Phill",
+        hasPromoCode:true,
+        group:"B",
+        hobbies:["Riding", "Fishing", "Drafting"]
+    }
+];
